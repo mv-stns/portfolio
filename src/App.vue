a5999dc723740c363be15147781a429edd303ae1
mv-stns <21103251+mv-stns@users.noreply.github.com>
2023-11-11 17:56:48 +0100

    Refactor project structure and styling
    - Added `postcss.config.js` to configure PostCSS plugins for tailwindcss and autoprefixer
    - Removed `assets/base.css`, `assets/logo.svg`, and `assets/main.css`
    - Created `src/components/Navigation.vue` to handle navigation links
    - Updated `src/App.vue` to use `Navigation.vue` component and apply tailwind CSS classes
    - Updated `src/main.ts` to import `tailwind.css` instead of `main.css`
    - Added `tailwind.config.js` for customizing tailwind CSS
    
    Note: This message assumes that the changes made to `postcss.config.js`, `tailwind.config.js`, and `src/components/Navigation.vue` are intentional and not accidental.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Navigation from '@/components/Navigation.vue'
 import '@/styles/tailwind.css'
 </script>
 
 <template>
-  <ul>
-    <li>
-      <RouterLink to="/about">AboutView</RouterLink>
-    </li>
-    <li>
-      <RouterLink to="/">Home</RouterLink>
-    </li>
-  </ul>
-  <RouterView />
+  <Navigation />
+  <RouterView class='h-full' />
 </template>
 
 <style>
