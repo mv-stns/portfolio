546e0cf32eb11820445a68104b35231a22eb4f7d
mv-stns <21103251+mv-stns@users.noreply.github.com>
2024-01-19 16:44:36 +0100

    Update Vite configuration, index.html, package.json, router, App.vue, and HomeView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,14 @@
 <template>
-  <div class="h-screen" :class="isDarkMode ? 'bg-zinc-950' : ''">
-    <!-- <Navigation />
-    <RouterView class="h-full" /> -->
-    <picture>
-      <source
-        media="(prefers-color-scheme: dark)"
-        srcset="https://readme.mv-stns.workers.dev?section=top&theme=dark"
-      />
-      <img
-        src="https://readme.mv-stns.workers.dev?section=top&theme=light"
-        width="100%"
-        height="20"
-        align="left"
-      />
-    </picture>
-    <a href="https://vaitschulis.com">
-      <picture>
-        <source
-          media="(prefers-color-scheme: dark)"
-          srcset="https://readme.mv-stns.workers.dev?section=link-website&theme=dark"
-          label="Visit"
-        />
-        <img
-          src="https://readme.mv-stns.workers.dev?section=link-website&theme=light&i=0"
-          alt="visit my website"
-          width="100"
-          height="18px"
-          align="left"
-        />
-      </picture>
-    </a>
-    <img src="data:null;," width="100%" height="0" align="left" alt="" />
-    <a href="https://twitter.com/MarcusSowieso">
-      <picture>
-        <source
-          media="(prefers-color-scheme: dark)"
-          srcset="https://readme.mv-stns.workers.dev?section=link-twitter&theme=dark"
-        />
-        <img
-          src="https://readme.mv-stns.workers.dev?section=link-twitter&theme=light&i=1"
-          alt="visit my Twitter/X profile"
-          width="100"
-          height="18"
-          align="left"
-        />
-      </picture>
-    </a>
-    <img src="data:null;," width="100%" height="0" align="left" alt="" />
-    <!-- <a href="https://www.instagram.com/mv-stnsg">
-  <picture>
-    <source media="(prefers-color-scheme: dark)" srcset="https://readme.mv-stns.workers.dev?section=link-instagram&theme=dark">
-    <img src="https://readme.mv-stns.workers.dev?section=link-instagram&theme=light&i=2" alt="visit my Instagram" width="100" height="18" align="left">
-  </picture>
-</a> -->
-    <img src="data:null;," width="100%" height="0" align="left" alt="" />
-    <picture>
-      <source
-        media="(prefers-color-scheme: dark)"
-        srcset="https://readme.mv-stns.workers.dev?section=fallback&theme=dark"
-      />
-      <img
-        src="https://readme.mv-stns.workers.dev?section=fallback&theme=light"
-        alt=""
-        width="420"
-        align="left"
-      />
-    </picture>
-    <picture>
-      <source
-        media="(prefers-color-scheme: dark)"
-        srcset="https://readme.mv-stns.workers.dev?section=main&theme=dark"
-      />
-      <img
-        src="https://readme.mv-stns.workers.dev?section=main&theme=light"
-        alt="I'm Marcus. A Germany-based Nerd."
-        width="100%"
-        height="290"
-        align="left"
-      />
-    </picture>
+  
+  <div class="container">
+    <router-view />
   </div>
 </template>
 
 <script>
 import '@/styles/tailwind.css'
+import router from '@/router'
+
 export default {
   data() {
     return {
---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <h1 class="text-4xl [font-family:'Geist',sans-serif]">Portfolio</h1>
+    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 md:p-6">
+      <Work
+        class="cursor-pointer"
+        v-for="work in works"
+        :key="work.id"
+        :image="work.image"
+        :title="work.title"
+        :date="new Date(work.date)"
+        @click="toggler = !toggler, slide = work.id"
+      />
+    </section>
+  </div>
+  <!-- set current image as lightbox -->
+  
+  <FsLightbox
+    :toggler="toggler"
+    :sources="works.map(work => work.image)"
+    :type="'image'"
+    :slide="slide"
+  />
+
+</template>
+
+<script>
+import Work from '@/views/work/Work.vue'
+import FsLightbox from 'fslightbox-vue/v3'
+import { ref } from 'vue'
+
+const works = [
+  {
+    "id": 1,
+    "image": "/images/works/work_01.png",
+    "title": "Folder Illustration",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet dolor libero, eget.",
+    "date": "2023-05-23"
+  },
+  {
+    "id": 2,
+    "image": "/images/works/work_02.png",
+    "title": "K&P Landingpage [WIP]",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet dolor libero, eget.",
+    "date": "2024-01-05"
+  },
+  {
+    "id": 3,
+    "image": "/images/works/work_03.png",
+    "title": "payify - Customer Empty State",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet dolor libero, eget.",
+    "date": "2023-12-20"
+  },
+  {
+    "id": 4,
+    "image": "/images/works/work_04.png",
+    "title": "payify - Sign In / Sign Up Page",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet dolor libero, eget.",
+    "date": "2023-07-17"
+  },
+  {
+    "id": 5,
+    "image": "/images/works/work_05.png",
+    "title": "CHAIN - NFT Marketplace (University Project)",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet dolor libero, eget.",
+    "date": "2023-06-12"
+  },
+  {
+    "id": 6,
+    "image": "/images/works/work_06.png",
+    "title": "search2fit - Sign In / Sign Up Page",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet dolor libero, eget.",
+    "date": "2022-12-16"
+  },
+  {
+    "id": 7,
+    "image": "/images/works/work_07.png",
+    "title": "taimr - Blog Entries & Categories CMS",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet dolor libero, eget.",
+    "date": "2023-04-25"
+  },
+  {
+    "id": 8,
+    "image": "/images/works/work_08.png",
+    "title": "PraxisBooster - About Us Page",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet dolor libero, eget.",
+    "date": "2023-06-02"
+  },
+]
+export default {
+  components: {
+    Work,
+    FsLightbox
+  },
+  data() {
+    return {
+      works,
+      toggler: ref(false)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
